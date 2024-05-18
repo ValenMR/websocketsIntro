c376d6a7f5678c6c5aa7a11fab96640e21b87d2e
ValenMR <valentina.munozr1@udea.edu.co>
2024-05-18 18:45:19 -0500

    Creando el websocket con binance

---
+++ app.js
@@ -0,0 +1,6 @@
+let ws = new WebSocket('wss://stream.binance.com:9443/ws/etheur@trade');
+let stockPriceElement = document.getElementById('stock-price');
+ws.onmessage=(event) => {
+    let stockObject = JSON.parse(event.data);
+    stockPriceElement.innerText = stockObject.p;
+};
